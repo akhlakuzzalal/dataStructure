dab655fbccf3f0e09993134d5b65c3d02555c21a
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-10-26 10:38:05 +0600

    topological sort

---
+++ Graph/topologicalSort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int v, e;
+    cout << "enter vertex and edges" << endl;
+    cin >> v >> e;
+    cout << "node :" << v << "edges :" << e << endl;
+    vector<vector<int>> adj;
+    vector<int> inDegree(v, 0);
+    for (int i = 0; i < e; i++)
+    {
+        int n, edge;
+        cout << "enter node and edges" << endl;
+        cin >> n >> edge;
+        adj[n].push_back(edge);
+        inDegree[edge]++;
+    }
+
+    queue<int> Q;
+    for (int i = 0; i < v; i++)
+    {
+        if (inDegree[i] == 0)
+            Q.push(i);
+    }
+
+    while (!Q.empty())
+    {
+        int x = Q.front();
+        Q.pop();
+        cout << x << " ";
+        for (auto it : adj[x])
+        {
+            inDegree[x]--;
+            if (inDegree[it] == 0)
+                Q.push(it);
+        }
+    }
+
+    return 0;
+}
